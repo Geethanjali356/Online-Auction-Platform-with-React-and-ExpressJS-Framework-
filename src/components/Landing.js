dbd3d3f4aa69fd1e3665e254d9e1f30d0e546948
Geethanjali356 <156283145+Geethanjali356@users.noreply.github.com>
2025-02-21 18:46:45 +0530

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,12 @@
-
-function Landing() 
-{
-return(<>
-<div className='content'>
-          <h2>Welcome to Auction App</h2>
-          <p>
-          An auction is usually a process of buying and selling goods or services by offering them up for bids, taking bids, and then selling the item to the highest bidder or buying the item from the lowest bidder. Some exceptions to this definition exist and are described in the section about different types.
-          </p>
-          </div>
-          </>
-)
+function Landing() {
+  return (
+    <div className='landing-container'>
+      <h2 className='landing-title'>Welcome to Auction App</h2>
+      <p className='landing-text'>
+        An auction is a process of buying and selling goods or services by offering them up for bids,
+        taking bids, and then selling the item to the highest bidder.
+      </p>
+    </div>
+  );
 }
-
-export default Landing
+export default Landing;
